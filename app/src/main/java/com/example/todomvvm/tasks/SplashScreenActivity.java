f3949646b46ec823508f2865ca42cb61c97e4847
Devesh <deveshmdr2@gmail.com>
2020-06-08 08:16:44 +0545

    Drawer menu

---
+++ app/src/main/java/com/example/todomvvm/tasks/SplashScreenActivity.java
@@ -0,0 +1,12 @@
+package com.example.todomvvm.tasks;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SplashScreenActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
